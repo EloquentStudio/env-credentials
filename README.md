6fdc273d5a880317c394b29a0c11ec3df025f6c8
Jiren Patel <jirenpatel@gmail.com>
2020-08-02 17:39:38 +0530

    cli to store env key and updated readme

--- README.md
+++ README.md
@@ -1,42 +1,75 @@
 ### env-credentials
 
-Encrypt applicattion environment credentials manager and export ENV varaiable on load.
+Encrypt JSON object of application environment credentials and configuration manager and export ENV varaiable on load.
 
 #### How to use?
 
   Help command.
-  
+
   ```shell
   npx env-credentials --help
   ```
-  
 
-  1. Generate master key
-  
+  1. Generate master key ans save into environment specific file.
+
+  Default environment is development.
+
   ```shell
   npx env-credentials master-key
-  
+
+  Saved in a credentials directory credentials/development.key
+
   8a44911940b02fbc659277d89af48cb2628b31f6bc63b87b433da46966af6aec
 
   To use 'export APP_MASTER_KEY=8a44911940b02fbc659277d89af48cb2628b31f6bc63b87b433da46966af6aec'
+  OR Save in a credentials directory i.e credentials/development.key
   ```
 
+  Generate environment specific key and save.
+  ```shell
+  npx env-credentials master-key -e production
+
+  Saved in a credentials directory credentials/production.key
+
+  48260a6dbe1bc8173e1ef5486ae30805e024500e7142ee8c7096536eab319bf7
+
+  To use 'export APP_MASTER_KEY=48260a6dbe1bc8173e1ef5486ae30805e024500e7142ee8c7096536eab319bf7'
+  OR Save in a credentials directory i.e credentials/production.key
+  ```
+
+  Generate key without saving it.
+
+  ```shell
+  npx env-credentials master-key -e production -s false
+  ```
+
+
   2. Create / Update credentials
-  
+
   Default environment is `development`
-  
+
+  ```shell
+  npx env-credentials edit # loads app master key from 'credentials/development.key' file.
+  ```
+
   ```shell
   APP_MASTER_KEY=8a44911940b02fbc659277d89af48cb2628b31f6bc63b87b433da46966af6aec npx env-credentials edit
   ```
- 
+
+  Edit by environment option.
+
+  ```shell
+  npx env-credentials edit -e production # loads app master key from 'credentials/production.key' file.
+  ```
+
   ```shell
   APP_MASTER_KEY=252949256031ababb811706b4dcf662577e1b19d1980ef0c8b1bdfef13feba36 npx env-credentials edit -e production
   ```
 
   3. Load credentials
-  
+
   Export `APP_MASTER_KEY` and `NODE_ENV`. `NODE_ENV` is default to `development`.
-  
+
   ```javascript
     require('env-credentials').load()
   ```
--- bin/cli.js
+++ bin/cli.js
@@ -1,17 +1,72 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
+const path = require('path');
 const {
   generateKey,
   edit,
   encryptFile
 } = require('../src');
 
-require('yargs')
-  .command('master-key', 'Generate master encryption key', () => {}, (_a) => {
-    const key = generateKey()
+function writeKey(credentialsDir, file, key) {
+  const keyFile = path.join(credentialsDir, file);
 
-    console.log('\n  ', key, '\n')
-    console.log(`   To use 'export APP_MASTER_KEY=${key}'\n`)
+  if (fs.existsSync(keyFile)) {
+    throw new Error(`\n error: ${keyFile} file is already exist. \n`);
+  }
+
+  if (!fs.existsSync(credentialsDir)) {
+    fs.mkdirSync(credentialsDir);
+  }
+
+  fs.writeFileSync(keyFile, key);
+
+  const gitignoreLine = `${credentialsDir}/*.key`;
+  let gitignoreContent;
+
+  if (fs.existsSync('.gitignore')) {
+    gitignoreContent = fs.readFileSync('.gitignore').toString();
+
+    if (!gitignoreContent.includes(gitignoreLine)) {
+      fs.appendFileSync('.gitignore', `\n${gitignoreLine}\n`);
+    }
+  } else {
+    fs.writeFileSync('.gitignore', `${gitignoreLine}\n`);
+  }
+}
+
+require('yargs')
+  .command('master-key [credentials] [environment] [save]', 'Generate master encryption key', (yargs) => {
+    yargs.positional('credentials', {
+        type: 'string',
+        default: 'credentials',
+        describe: 'Path of the credentials where key will be saved. (i.e credentials)'
+      })
+      .positional('environment', {
+        type: 'string',
+        default: (process.env.NODE_ENV || 'development'),
+        describe: 'The name of the application environment. i.e production.'
+      })
+      .positional('save', {
+        type: 'boolean',
+        default: true,
+        describe: 'Flag to save into file'
+      })
+      .alias('c', 'credentials')
+      .alias('e', 'environment')
+      .alias('s', 'save')
+  }, (argv) => {
+    const key = generateKey();
+
+    if (argv.save) {
+      writeKey(argv.credentials, `${argv.environment}.key`, key);
+      console.log(`\n    Saved in a credentials directory ${argv.credentials}/${argv.environment}.key\n`);
+    }
+
+    console.log('\n   ', key, '\n');
+    console.log(`
+    To use 'export APP_MASTER_KEY=${key}'
+    OR Save in a credentials directory i.e credentials/${argv.environment}.key\n `);
   })
   .command('edit [environment] [credentials] [masterkey]', 'Edit credentials', (yargs) => {
     yargs.positional('environment', {
@@ -57,7 +112,12 @@ require('yargs')
       masterKey: argv.masterkey
     });
 
-    console.log(`${argv.in} file encrypted and placed in '${outFilePath}'`);
+    console.log(`
+    $ {
+      argv.in
+    }
+    file encrypted and placed in '${outFilePath}'
+    `);
   })
   .help()
   .argv
